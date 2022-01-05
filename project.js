62e1f6b9f829637f4993c52ec9eb5d77c9bd6b20
Millan <munenenjue18@gmail.com>
2022-01-05 19:00:49 +0300

    close

--- project.js
+++ project.js
@@ -3,7 +3,7 @@ function ValidateEmail(mail)
  if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(myForm.emailAddr.value))
   {
     return (true)
-  }
+  }else
     alert("You have entered an invalid email address!")
     return (false)
 }
